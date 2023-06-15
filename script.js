3c54237f6ef8e999273e4e2f62c1103ecce2267c
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-06-15 20:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+	let stng = str.toLowerCase();
 	let newStr = "";
 	for(let j=str.length-1; j>=0; j--)
 		{
 			newStr += str[j].toLowerCase();
 		}
-	return str === newStr ? true : false;
+	return stng === newStr ? true : false;
 }
 module.exports = palindrome
